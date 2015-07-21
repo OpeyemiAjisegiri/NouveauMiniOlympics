43d3af5ca9c92b40d8a13301e02fc39403270863
Opeyemi <contact.oajisegiri@gmail.com>
2015-07-20 20:27:09 -0400

    Added more profile features

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
   private
 
     def user_params
-    	params.require(:user).permit(:email)
+    	params.require(:user).permit(:id, :email, profile_attributes: [:name, 
+    		:address, :sex] )
     end
 end
