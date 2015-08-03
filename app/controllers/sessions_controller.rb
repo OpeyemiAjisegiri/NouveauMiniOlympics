0e8c73d9eb140d128afe9a4305ee0ae5c0bfdaa2
Opeyemi <contact.oajisegiri@gmail.com>
2015-08-03 14:56:48 -0400

    Revert "Finish user edit, update, index, and destroy action; but with an little eror in the test, the user's name"
    This reverts commit dd86d91d86925483ed65c760c581445b2ba6a38b.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,14 @@ class SessionsController < ApplicationController
   	if user && user.authenticate(params[:session][:password])
   		log_in user
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-  		redirect_back_or user
+  		redirect_to user
+    ### The above code works but the one below uses the instance variable i.e. self
+    ### but "?user" throws errors into my test that i dont know how to solve yet
+    #?user = User.find_by(email: params[:session][:email].downcase)
+    #if ?user && ?user.authenticate(params[:session][:password])
+    #  log_in ?user
+    #  params[:session][:remember_me] == '1' ? remember(?user) : forget(?user)
+    #  redirect_to ?user
   	else 
   		flash.now[:danger] = 'Invalid email/password combination'
   		render 'new'
--- db/schema.rb
+++ db/schema.rb
@@ -22,9 +22,8 @@ ActiveRecord::Schema.define(version: 20150720224924) do
     t.string   "email"
     t.string   "password_digest"
     t.string   "remember_digest"
-    t.boolean  "admin",           default: false
-    t.datetime "created_at",                      null: false
-    t.datetime "updated_at",                      null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
